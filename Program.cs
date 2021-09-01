4eb3093d4892c90d29c879e24425840d01764787
Parviz Karimov <bezlpk@gmail.com>
2021-09-01 13:33:02 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Play();
+            player.Pause();
+            player.Record();
+            player.Stop();
+        }
+        interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        }
+        interface IRecordable
+        {
+            void Record();
+            void Pause();
+            void Stop();
+        }
+        class Player : IPlayable, IRecordable
+        {
+            public void Play() => System.Console.WriteLine("Playing...");
+            public void Record() => System.Console.WriteLine("Recording...");
+            public void Pause() => System.Console.WriteLine("Paused");
+            public void Stop() => System.Console.WriteLine("Stoped");
+        }
+    }
+}
